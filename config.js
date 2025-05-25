bacd7214600524b17f4e288f283260eeb0817a6a
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-25 00:55:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 {
-  "botToken": "MTM3MTc1NDg4MzgyNDIyNjM5NA.GEzETS.MMYOdR0QO__5RGTiR84QbcibtgC3gRDOSD4Mcc",
-  "openaiApiKey": "COLE_AQUI_SUA_OPENAI_API_KEY",
+  "botToken": "MTM3MTc1NDg4MzgyNDIyNjM5NA.GZrtwS.j4jAqhyHrkbBC7KRMseD9cxJv7KfgsBT-K40TQ",
+  "openaiApiKey": "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",
   "webhookUrl": "https://discord.com/api/webhooks/SEU_WEBHOOK_AQUI"
 }
