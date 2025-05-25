a4e01b8bc8465ca3380d798c320767126e740059
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-25 00:51:08 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+{
+  "botToken": "MTM3MTc1NDg4MzgyNDIyNjM5NA.GEzETS.MMYOdR0QO__5RGTiR84QbcibtgC3gRDOSD4Mcc",
+  "openaiApiKey": "COLE_AQUI_SUA_OPENAI_API_KEY",
+  "webhookUrl": "https://discord.com/api/webhooks/SEU_WEBHOOK_AQUI"
+}
